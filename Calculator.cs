1739b7bb4ebb2e981cc61f181ba271eb366da306
Luzefiru <ian.deejay.iv@gmail.com>
2024-02-09 10:27:54 +0800

    refactor: use class auto properties syntax

--- Calculator.cs
+++ Calculator.cs
@@ -2,34 +2,33 @@ namespace CalculatorQuiz2
 {
     public class Calculator
     {
-        decimal currentValue;
-
-        public decimal GetCurrentValue() {
-            return currentValue;
+        public decimal CurrentValue
+        {
+            get; set;
         }
 
-         public void SetCurrentValue(decimal newVal) {
-            this.currentValue = newVal;
+        public void Add(decimal operand)
+        {
+            decimal res = this.CurrentValue + operand;
+            this.CurrentValue = res;
         }
 
-        public void Add(decimal operand) {
-            decimal res = currentValue + operand;
-            SetCurrentValue(res);
+        public void Subtract(decimal operand)
+        {
+            decimal res = this.CurrentValue - operand;
+            this.CurrentValue = res;
         }
 
-        public void Subtract(decimal operand) {
-            decimal res = currentValue - operand;
-            SetCurrentValue(res);
+        public void Multiply(decimal operand)
+        {
+            decimal res = this.CurrentValue * operand;
+            this.CurrentValue = res;
         }
 
-        public void Multiply(decimal operand) {
-            decimal res = currentValue * operand;
-            SetCurrentValue(res);
-        }
-
-        public void Divide(decimal operand) {
-            decimal res = currentValue / operand;
-            SetCurrentValue(res);
+        public void Divide(decimal operand)
+        {
+            decimal res = this.CurrentValue / operand;
+            this.CurrentValue = res;
         }
     }
 }
--- UserInterface.cs
+++ UserInterface.cs
@@ -1,9 +1,10 @@
 using CalculatorQuiz2;
 
-public sealed class UserInterface {
+public sealed class UserInterface
+{
     private static readonly UserInterface instance = new UserInterface();
     private static Calculator calculator = new Calculator();
-     decimal operand;
+    decimal operand;
 
     private UserInterface() { } // Private constructor to prevent external creation
 
@@ -13,40 +14,43 @@ public sealed class UserInterface {
         get { return instance; }
     }
 
-       
+
     public void Start()
     {
-        int command = default; 
-        
+        int command = default;
+
         while (command != 5)
-        {   
+        {
             Console.WriteLine("================= ENTER YOUR CALCULATION ====================");
             GetOperands();
-            GetCommand(out command); 
+            GetCommand(out command);
 
             switch (command)
             {
                 case 1:
-                   calculator.Add(operand);
-                   Console.WriteLine("The answer is " + Math.Round(calculator.GetCurrentValue(), 2));
+                    calculator.Add(operand);
+                    Console.WriteLine("The answer is " + Math.Round(calculator.CurrentValue, 2));
 
                     break;
                 case 2:
                     calculator.Subtract(operand);
-                   Console.WriteLine("The answer is " + Math.Round(calculator.GetCurrentValue(), 2));
+                    Console.WriteLine("The answer is " + Math.Round(calculator.CurrentValue, 2));
 
                     break;
                 case 3:
                     calculator.Multiply(operand);
-                   Console.WriteLine("The answer is " + Math.Round(calculator.GetCurrentValue(), 2));
+                    Console.WriteLine("The answer is " + Math.Round(calculator.CurrentValue, 2));
 
                     break;
                 case 4:
-                    if(this.operand == 0) {
+                    if (this.operand == 0)
+                    {
                         Console.WriteLine("Math Error. Please input another calculation.");
-                    } else {
+                    }
+                    else
+                    {
                         calculator.Divide(operand);
-                        Console.WriteLine("The answer is " + Math.Round(calculator.GetCurrentValue(), 2));
+                        Console.WriteLine("The answer is " + Math.Round(calculator.CurrentValue, 2));
                     }
 
                     break;
@@ -61,24 +65,28 @@ public sealed class UserInterface {
 
     }
 
-    public bool GetOperands() {
+    public bool GetOperands()
+    {
         Console.WriteLine("Input your first operand:");
         string? input = Console.ReadLine();
         decimal temp;
 
-        while(decimal.TryParse(input, out temp) == false && input?.Equals("ans") != true){
+        while (decimal.TryParse(input, out temp) == false && input?.Equals("ans") != true)
+        {
             Console.WriteLine("Input is invalid. Please input another number:");
             input = Console.ReadLine();
         }
 
-        if(input?.Equals("ans") != true) {
-            calculator.SetCurrentValue(temp);
-        } 
-         
-         Console.WriteLine("Input your second operand:");
+        if (input?.Equals("ans") != true)
+        {
+            calculator.CurrentValue = temp;
+        }
+
+        Console.WriteLine("Input your second operand:");
         input = Console.ReadLine();
 
-        while(decimal.TryParse(input, out this.operand) == false){
+        while (decimal.TryParse(input, out this.operand) == false)
+        {
             Console.WriteLine("Input is invalid. Please input another number:");
             input = Console.ReadLine();
         }
@@ -86,7 +94,7 @@ public sealed class UserInterface {
         return true;
     }
     public bool GetCommand(out int command)
-    {   
+    {
         Console.WriteLine("Choose an Operation:");
         Console.WriteLine("1 - Add");
         Console.WriteLine("2 - Subtract");
@@ -101,7 +109,7 @@ public sealed class UserInterface {
         if (isValidInput)
         {
             command = parsedInput;
-     
+
             return true;
         }
         else
